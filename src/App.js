a620d6a3e403a520c810ccd9dac30125a6b17fbf
Munsterberg <jetrussell93@gmail.com>
2016-11-27 20:08:38 -0500

    setup redux structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Router, browserHistory} from 'react-router';
+import {Provider} from 'react-redux';
 
+import configureStore from './store/configureStore';
 import Routes from './Routes';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+const store = configureStore();
 
 ReactDOM.render(
-    <Router history={browserHistory} routes={Routes} />,
+    <Provider store={store}>
+        <Router history={browserHistory} routes={Routes} />
+    </Provider>,
     document.getElementById('app')
 );
